03dbcd0fe0b7f9fa4ac7435f319d546e65099d6a
Aaleeyah <samanthastar87@gmail.com>
2022-09-19 10:09:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const commands = new Collection();
 const aliases = new Collection();
 const slashcmds = new Collection();
 
+client.commands = new Collection();
 // Generate a cache of client permissions for pretty perm names in commands.
 const levelCache = {};
 for (let i = 0; i < permLevels.length; i++) {
